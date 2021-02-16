006bd2c3c06b4ccb69ab7421163bdb6fff26c2b6
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-17 03:12:02 +0600

    image hover styled

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
 import { Card, Col } from "react-bootstrap";
 
 const Photo = ({ photo }) => {
+  const [show, setShow] = useState(false);
+  console.log(show);
   return (
-    <Col md={4} className="p-2">
-      <Card>
+    <Col md={4} className="p-2 align-content-center">
+      <Card
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
         <Card.Img src={photo.photo} />
+        {show && (
+          <Card.ImgOverlay className="d-flex align-content-center flex-wrap justify-content-center bg-blur">
+            <Card.Title as="h1">{photo.title}</Card.Title>
+          </Card.ImgOverlay>
+        )}
       </Card>
     </Col>
   );
